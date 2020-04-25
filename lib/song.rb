9e1625409f06fa03d25e6279e6f44227a8471c92
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-25 00:20:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,9 @@ def self.all
   @@all
 end
 
-def artist=(artist)
-  @artist = artist
-  
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)
+
 end
 
 def self.new_by_filename(file_name)
